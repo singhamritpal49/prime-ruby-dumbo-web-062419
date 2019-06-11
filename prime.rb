cfe1c354616c5349279b83f2fa2baa8378ee414c
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-11 05:16:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,7 @@
 #def prime?(num)
 #return false if num < 2
 #end
-def is_prime?(num)
-  (2.Math.sqrt(num)).each do |i|
-    return false if num % i == 0
-  end
-  true
+def prime?(num)
+return false if num < 2
+(2..num-1).none? {|n| num % n == 0}
 end
